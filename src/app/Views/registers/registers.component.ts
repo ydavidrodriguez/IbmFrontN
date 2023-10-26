fb7c3533bc41393b2fb00644bc61b952998ca6ce
Edwar Garcia <garciapatinoedwar26@gmail.com>
2023-10-26 18:05:28 -0500

    Created an HTML template for registers

--- src/app/Views/registers/registers.component.ts
+++ src/app/Views/registers/registers.component.ts
@@ -6,5 +6,11 @@ import { Component } from '@angular/core';
   styleUrls: ['./registers.component.css']
 })
 export class RegistersComponent {
+  Datos = [
+    {nombre: 'Juan Acosta', idUsuario: 'C545645', cliente: 'Tvall', reporte: '00001', correo: 'juan@correo.com', cargo: 'Empleado' ,editar:'ver detalles'},
+    {nombre: 'Mario Duarte', idUsuario: 'C369847', cliente: 'AllSa', reporte: '00020', correo: 'mario@correo.com', cargo: 'Aprobador' ,editar:'ver detalles'},
+    
+  ];
 
+  columnasAMostrar = ['nombre', 'idUsuario', 'cliente', 'reporte', 'correo', 'cargo', 'editar'];
 }
